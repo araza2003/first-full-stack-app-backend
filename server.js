365e97c4010fd850ed473128008d1f76b4f2038a
araza2003 <ahmedrazaspan@gmail.com>
2025-03-11 01:52:00 +0500

    new commit

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from "express"; 
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -11,7 +11,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+
+// Enable CORS for all origins
+app.use(cors({ 
+  origin: "*", 
+  methods: "GET, POST, PUT, DELETE"
+}));
 
 // Routes
 app.use("/api/users", userRoutes);
@@ -21,9 +26,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch(err => console.log(err));
 
-  
-
-  app.get("/", (req, res) => {
-    res.send("Backend is running!");
-  });
-  
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
